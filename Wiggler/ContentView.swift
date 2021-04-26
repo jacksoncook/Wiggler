26f988b246ee54ed6e3432bd1d4075f2baec41cc
jacksoncook <jacksoncook73@gmail.com>
2021-04-25 20:23:08 -0700

    Replacing accidental iOS intializataion with MacOS

---
+++ Wiggler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wiggler
+//
+//  Created by Jackson Cook on 4/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wiggler/WigglerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WigglerApp.swift
+//  Wiggler
+//
+//  Created by Jackson Cook on 4/25/21.
+//
+
+import SwiftUI
+
+@main
+struct WigglerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
